eef2cfe6361d21a7e41e78ef30e6cbe45dfac268
Dylan <11531208+Dill-Lan@users.noreply.github.com>
2024-11-03 09:48:04 +1300

    Configure ktor client part of service

--- counter-client/src/main/kotlin/games/august/counter/service/api/CounterApi.kt
+++ counter-client/src/main/kotlin/games/august/counter/service/api/CounterApi.kt
@@ -6,11 +6,18 @@ import games.august.counter.service.api.model.GetCountAggregateRequest
 import games.august.counter.service.api.model.UpdateCounterRequest
 
 internal interface CounterApi {
-    suspend fun updateCounter(updateCounterRequest: UpdateCounterRequest): Result<Unit>
+    suspend fun updateCounter(
+        apiToken: String,
+        updateCounterRequest: UpdateCounterRequest,
+    ): Result<Unit>
 
-    suspend fun batchUpdateCounters(batchUpdateCounterRequest: BatchUpdateCounterRequest): Result<Unit>
+    suspend fun batchUpdateCounters(
+        apiToken: String,
+        batchUpdateCounterRequest: BatchUpdateCounterRequest,
+    ): Result<Unit>
 
     suspend fun getCountAggregate(
+        apiToken: String,
         tag: String,
         updateCounterRequest: GetCountAggregateRequest,
     ): Result<CounterAggregate>
--- counter-client/src/main/kotlin/games/august/counter/service/api/DefaultCounterApi.kt
+++ counter-client/src/main/kotlin/games/august/counter/service/api/DefaultCounterApi.kt
@@ -4,22 +4,96 @@ import games.august.counter.service.api.model.BatchUpdateCounterRequest
 import games.august.counter.service.api.model.CounterAggregate
 import games.august.counter.service.api.model.GetCountAggregateRequest
 import games.august.counter.service.api.model.UpdateCounterRequest
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.headers
+import io.ktor.client.request.request
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
-internal class DefaultCounterApi : CounterApi {
+internal class DefaultCounterApi(
+    private val httpClient: HttpClient =
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                        isLenient = true
+                    },
+                )
+            }
+        },
+) : CounterApi {
     private val baseUrl = "https://counter-np.august.games"
 
-    override suspend fun updateCounter(updateCounterRequest: UpdateCounterRequest): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun updateCounter(
+        apiToken: String,
+        updateCounterRequest: UpdateCounterRequest,
+    ): Result<Unit> =
+        runCatching {
+            val response =
+                httpClient.request("$baseUrl/counter/update") {
+                    method = HttpMethod.Post
+                    headers {
+                        append(HttpHeaders.Accept, "application/json")
+                        append("X-Api-Key", apiToken)
+                        append(HttpHeaders.UserAgent, "ktor client")
+                    }
+                    setBody(updateCounterRequest)
+                }
+            when {
+                response.status.isSuccess() -> Unit
+                else -> error("Failed to update counter for tag \"${updateCounterRequest.tag}\": ${response.bodyAsText()}")
+            }
+        }
 
-    override suspend fun batchUpdateCounters(batchUpdateCounterRequest: BatchUpdateCounterRequest): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun batchUpdateCounters(
+        apiToken: String,
+        batchUpdateCounterRequest: BatchUpdateCounterRequest,
+    ): Result<Unit> =
+        runCatching {
+            val response =
+                httpClient.request("$baseUrl/counter/batch-update") {
+                    method = HttpMethod.Post
+                    headers {
+                        append(HttpHeaders.Accept, "application/json")
+                        append("X-Api-Key", apiToken)
+                        append(HttpHeaders.UserAgent, "ktor client")
+                    }
+                    setBody(batchUpdateCounterRequest)
+                }
+            when {
+                response.status.isSuccess() -> Unit
+                else -> error("Failed to batch update counters: ${response.bodyAsText()}")
+            }
+        }
 
     override suspend fun getCountAggregate(
+        apiToken: String,
         tag: String,
         updateCounterRequest: GetCountAggregateRequest,
-    ): Result<CounterAggregate> {
-        TODO("Not yet implemented")
-    }
+    ): Result<CounterAggregate> =
+        runCatching {
+            val response =
+                httpClient.request("$baseUrl/$tag/get") {
+                    method = HttpMethod.Post
+                    headers {
+                        append(HttpHeaders.Accept, "application/json")
+                        append("X-Api-Key", apiToken)
+                        append(HttpHeaders.UserAgent, "ktor client")
+                    }
+                    setBody(updateCounterRequest)
+                }
+            when {
+                response.status.isSuccess() -> response.body()
+                else -> error("Failed to get count aggregate for tag \"$tag\": ${response.bodyAsText()}")
+            }
+        }
 }
